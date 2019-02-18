7dc6324c232167ac015b169cd5e80a54ac1c91b6
田中敏之 <gakitherock19841102@gmail.com>
2019-02-19 00:34:28 +0900

    掲示板削除機能の作成
    と、パーシャルによるビューの再利用

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -9,12 +9,33 @@ class BoardsController < ApplicationController
   end
 
   def create
-    Board.create(board_params)
+    board = Board.create(board_params)
+
+    redirect_to board
   end
 
   def show
     @board = Board.find(params[:id])
   end
+
+  def edit
+    @board = Board.find(params[:id])
+  end
+
+  def update
+    board = Board.find(params[:id])
+    board.update(board_params)
+
+    redirect_to board
+  end
+
+  def destroy
+    board = Board.find(params[:id])
+    board.delete
+
+    redirect_to boards_path
+  end
+
 
   private
 
