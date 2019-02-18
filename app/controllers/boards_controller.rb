0b7ffff46e7365d2528ff13c076f4664553ec66d
田中敏之 <gakitherock19841102@gmail.com>
2019-02-19 00:47:05 +0900

    コントローラーのフィルター機能

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,5 @@
 class BoardsController < ApplicationController
+  before_action :set_target_board, only: %i[show edit update destroy]
 
   def index
     @boards = Board.all
@@ -10,32 +11,24 @@ class BoardsController < ApplicationController
 
   def create
     board = Board.create(board_params)
-
     redirect_to board
   end
 
   def show
-    @board = Board.find(params[:id])
   end
 
   def edit
-    @board = Board.find(params[:id])
   end
 
   def update
-    board = Board.find(params[:id])
-    board.update(board_params)
-
-    redirect_to board
+    @board.update(board_params)
+    redirect_to @board
   end
 
   def destroy
-    board = Board.find(params[:id])
-    board.delete
-
+    @board.delete
     redirect_to boards_path
   end
-
 
   private
 
@@ -43,4 +36,7 @@ class BoardsController < ApplicationController
     params.require(:board).permit(:name, :title, :body)
   end
 
+  def set_target_board
+    @board = Board.find(params[:id])
+  end
 end
