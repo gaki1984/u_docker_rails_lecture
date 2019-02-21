19bb1afdbc8b643009a893ebfa5e7f519ef52ab5
田中敏之 <gakitherock19841102@gmail.com>
2019-02-21 21:18:24 +0900

    annotate gemを追加

--- Gemfile
+++ Gemfile
@@ -57,6 +57,7 @@ group :development do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'pry-byebug', '~> 3.6.0'
+  gem 'annotate'
 end
 
 group :test do
--- app/models/board.rb
+++ app/models/board.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: boards
+#
+#  id         :bigint(8)        not null, primary key
+#  name       :string
+#  title      :string
+#  body       :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Board < ApplicationRecord
   validates :name, presence: true, length: { maximum: 10 }
   validates :title, presence: true, length: { maximum: 30 }
