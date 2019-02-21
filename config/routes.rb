3535957477164ed5a22f4c1f57f44233ac7a2482
田中敏之 <gakitherock19841102@gmail.com>
2019-02-21 21:37:49 +0900

    CommentsController作成
    ルートも追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'comments/create'
+  get 'comments/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :boards
+  resources :comments, only: %i[create destroy]
 end
